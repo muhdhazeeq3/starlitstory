0cdef6e43e0c20b15a7c9c47ebbc7ae111006c14
muhdhazeeq3 <hazeeq07@gmail.com>
2022-07-08 14:26:02 +0800

    test

---
+++ src/app/starlitstory-homepage/starlitstory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './starlitstory.component.html',
+  styleUrls: ['./starlitstory.component.scss']
+})
+export class AppComponent {
+  title = 'starlitstory';
+}
