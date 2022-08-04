249736cb8b00413fcbf465dbd823b63a86da100a
muhdhazeeq3 <hazeeq07@gmail.com>
2022-08-04 14:48:58 +0800

    Add firebase function

--- src/app/starlitstory-homepage/starlitstory.component.ts
+++ src/app/starlitstory-homepage/starlitstory.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'test',
   templateUrl: './starlitstory.component.html',
   styleUrls: ['./starlitstory.component.scss']
 })
-export class AppComponent {
+export class SLSComponent {
   title = 'starlitstory';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'starlitstory-576de',
+    appId: '1:771291199891:web:29e391f80321c07a8cd066',
+    databaseURL: 'https://starlitstory-576de-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'starlitstory-576de.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyDWsOvL4S5czsiKDRwdjM4n70jopdxzwZo',
+    authDomain: 'starlitstory-576de.firebaseapp.com',
+    messagingSenderId: '771291199891',
+    measurementId: 'G-WSS5785KYD',
+  },
   production: false
 };
 
